90b24c43ff20f6e36e1ae0fb7d772f64a1a3cab7
Dezzy12 <desmondneba134@gmail.com>
2023-03-28 08:01:04 +0100

    initial commit

--- src/main/java/com/dezzy/smst/controller/StudentController.java
+++ src/main/java/com/dezzy/smst/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.dezzy.smst.controller;
 
+import com.dezzy.smst.model.Student;
 import com.dezzy.smst.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,6 +22,20 @@ public class StudentController {
     public String getAllStudents(Model model){
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createNewStudent(Model model){
+//        creating student object
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String createStudent(@ModelAttribute("student") Student student){
+        studentService.createStudent(student);
+        return "redirect:/students";
     }
 
 
